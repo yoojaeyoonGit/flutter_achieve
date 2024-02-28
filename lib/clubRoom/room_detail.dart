1f4df8508e7180783971ef004a4bf982de055e2d
yoojaeyoonGit <jaeyoon321@naver.com>
2024-02-28 23:59:46 +0900

    feat: 시간 format 설정

--- lib/clubRoom/room_detail.dart
+++ lib/clubRoom/room_detail.dart
@@ -20,14 +20,9 @@ class _RoomDetailState extends State<RoomDetail> {
 
   @override
   Widget build(BuildContext context) {
-    double width = MediaQuery
-        .of(context)
-        .size
-        .width;
-    double height = MediaQuery
-        .of(context)
-        .size
-        .height;
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    double textSize = width * 0.05;
 
     return Scaffold(
       appBar: AppBar(
@@ -60,66 +55,15 @@ class _RoomDetailState extends State<RoomDetail> {
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    timeSelectButton(context, "날짜"),
-                    SizedBox(
-                      width: width / 5,
-                    ),
-                    CupertinoButton(
-                      onPressed: () {
-                        showCupertinoModalPopup<void>(
-                          context: context,
-                          builder: (BuildContext context) //{
-                          =>
-                              Container(
-                                height: height / 3,
-                                padding: const EdgeInsets.only(top: 1.0),
-                                margin: EdgeInsets.only(
-                                  bottom: MediaQuery
-                                      .of(context)
-                                      .viewInsets
-                                      .bottom,
-                                ),
-                                color: CupertinoColors.systemBackground
-                                    .resolveFrom(context),
-                                child: SafeArea(
-                                  top: false,
-                                  child: CupertinoDatePicker(
-                                    mode: CupertinoDatePickerMode.time,
-                                    initialDateTime: selectedDateAtTime,
-                                    onDateTimeChanged: (DateTime newDateTime) {
-                                      selectedDateAtTime = DateTime(
-                                          selectedDateForDate.year,
-                                          selectedDateForDate.month,
-                                          selectedDateForDate.day,
-                                          newDateTime.hour,
-                                          0,
-                                          0);
-
-                                      selectedFullDate = DateTime(
-                                          selectedDateForDate.year,
-                                          selectedDateForDate.month,
-                                          selectedDateForDate.day,
-                                          selectedDateAtTime.hour,
-                                          selectedDateAtTime.minute % 60);
-
-                                      setState(() {
-                                        durationForm = changeDurationForm(
-                                            selectedFullDate);
-                                      });
-                                    },
-                                  ),
-                                ),
-                              ),
-                        );
-                      },
-                      child: Text(
-                        durationForm = changeDurationForm(selectedFullDate),
-                        semanticsLabel: "$durationForm",
-                        style: const TextStyle(
-                          fontSize: 22.0,
-                        ),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 20.0),
+                      child: Icon(
+                        Icons.access_time_outlined,
+                        size: textSize,
                       ),
                     ),
+                    yearSelectButton(context, textSize),
+                    timeSelectedButton(textSize)
                   ],
                 )
               ],
@@ -130,19 +74,42 @@ class _RoomDetailState extends State<RoomDetail> {
     );
   }
 
-  String changeDurationForm(DateTime duration) {
-    return duration.toString().substring(0, 16);
+  String changeDurationForm(double left, DateTime duration) {
+    String amPmFormat = "";
+    String timeFormat = duration.toString().substring(11, 16);
+
+    print('${duration.hour}');
+    if (duration.hour < 13) {
+      amPmFormat = "오전";
+    }
+    amPmFormat = "오후";
+
+
+    return '${amPmFormat} ${timeFormat}';
   }
 
-  ElevatedButton timeSelectButton(BuildContext context, String buttonType) {
+  ElevatedButton yearSelectButton(BuildContext context, double textSize) {
     DateTime? selectedDate;
+    double width = MediaQuery.of(context).size.width;
+    double textSize = width * 0.05;
 
-    ElevatedButton yearSelectButton = ElevatedButton(
+
+    return ElevatedButton(
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(Colors.transparent),
+          elevation: MaterialStateProperty.all(0),
+          overlayColor: MaterialStateProperty.all(Colors.transparent),
+          shadowColor:
+              MaterialStateProperty.all(Colors.transparent), // 그림자 색을 투명으로 설정
+          padding: MaterialStateProperty.resolveWith((states) {
+            return EdgeInsets.only(right: width/20);
+          }),
+        ),
         onPressed: () async {
           selectedDate = await showDatePicker(
               context: context,
-              firstDate: DateTime(2024),
-              lastDate: DateTime(2025));
+              firstDate: DateTime(DateTime.now().year),
+              lastDate: DateTime(DateTime.now().year + 1));
 
           if (selectedDate != null) {
             selectedDateForDate = DateTime(
@@ -153,21 +120,113 @@ class _RoomDetailState extends State<RoomDetail> {
                 selectedDateForDate.year,
                 selectedDateForDate.month,
                 selectedDateForDate.day,
+                selectedDateForDate.weekday,
                 selectedDateAtTime.hour,
                 selectedDateAtTime.minute % 60,
-
               );
             });
           }
         },
-        child: Text(buttonType));
+        child: Text(
+          "${selectedDateForDate.year} - ${selectedDateForDate.month} - ${selectedDateForDate.day} - ${weekDayFormatting(selectedDateForDate.weekday)}",
+          style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.w400,
+              fontSize: textSize),
+        ));
+  }
 
-    return yearSelectButton;
+  String weekDayFormatting(int weekDay) {
+    if (weekDay == 1) {
+      return "월요일";
+    }
+
+    if (weekDay == 2) {
+      return "화요일";
+    }
+
+    if (weekDay == 3) {
+      return "수요일";
+    }
+
+    if (weekDay == 4) {
+      return "목요일";
+    }
+
+    if (weekDay == 5) {
+      return "금요일";
+    }
+
+    if (weekDay == 6) {
+      return "토요일";
+    }
+
+    return "일요일";
+  }
+
+  CupertinoButton timeSelectedButton(double textSize) {
+    double width = MediaQuery.of(context).size.width;
+
+    return CupertinoButton(
+      onPressed: () {
+        showCupertinoModalPopup<void>(
+          context: context,
+          builder: (BuildContext context) //{
+              =>
+              Container(
+            height: width/2,
+            padding: const EdgeInsets.only(top: 1.0),
+            margin: EdgeInsets.only(
+              bottom: MediaQuery.of(context).viewInsets.bottom,
+            ),
+            color: CupertinoColors.systemBackground.resolveFrom(context),
+            child: SafeArea(
+              top: false,
+              child: CupertinoDatePicker(
+                mode: CupertinoDatePickerMode.time,
+                initialDateTime: selectedDateAtTime,
+                onDateTimeChanged: (DateTime newDateTime) {
+                  selectedDateAtTime = DateTime(
+                      selectedDateForDate.year,
+                      selectedDateForDate.month,
+                      selectedDateForDate.day,
+                      newDateTime.hour,
+                      0,
+                      0);
+
+                  selectedFullDate = DateTime(
+                      selectedDateForDate.year,
+                      selectedDateForDate.month,
+                      selectedDateForDate.day,
+                      selectedDateAtTime.hour,
+                      selectedDateAtTime.minute % 60);
+
+                  setState(() {
+                    durationForm = changeDurationForm(width, selectedFullDate);
+                  });
+                },
+              ),
+            ),
+          ),
+        );
+      },
+      child: Text(
+        durationForm = changeDurationForm(width, selectedFullDate),
+
+
+        semanticsLabel: "$durationForm",
+        style: TextStyle(
+          fontWeight: FontWeight.w400,
+          color: Colors.black,
+          fontSize: textSize,
+        ),
+      ),
+    );
   }
 
   bool isTimeAllowed(Duration newDuration) {
-    Duration one = Duration(hours: 3, minutes: 30);
-    Duration two = Duration(hours: 2, minutes: 30);
+    Duration one = const Duration(hours: 3, minutes: 30);
+    Duration two = const Duration(hours: 2, minutes: 30);
 
     if (newDuration == one) {
       return false;
